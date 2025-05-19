81742a7de6732ab94c6e8c90dbff8a689ee8505b
jhuee <bse07210@gmail.com>
2025-05-19 14:50:12 +0900

    first commit

---
+++ Test2/src/student/StudentManagerTest.java
@@ -0,0 +1,34 @@
+package student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class StudentManagerTest {
+	
+	StudentManager sm;
+	
+    @BeforeEach
+    void setUp() {
+        sm = new StudentManager();
+    }
+
+	@Test
+	void testAddStudent() {
+		sm.addStudent("홍길동");
+        assertTrue(sm.hasStudent("홍길동")); 
+	}
+
+	@Test
+	void testRemoveStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHasStudent() {
+		fail("Not yet implemented");
+	}
+
+}
